05c351c988bf799b6e6106b9ef5e1c3ecf300d03
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-09-30 16:38:37 -0300

    feat: add CadastroCurso and RelatorioCurso components

---
+++ app/(root)/curso/cadastro/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CadastroCurso = () => {
+  return (
+    <div>CadastroCurso</div>
+  )
+}
+
+export default CadastroCurso
---
+++ app/(root)/curso/relatorio/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RelatorioCurso = () => {
+  return (
+    <div>RelatorioCurso</div>
+  )
+}
+
+export default RelatorioCurso
