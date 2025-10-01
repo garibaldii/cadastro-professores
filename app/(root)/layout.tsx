0f46c452a54677888154882e3f7cb0269edf6a6e
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-10-01 16:41:46 -0300

    style: update layout and page titles for improved clarity and user experience

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import { Toaster } from '@/components/ui/sonner'
 
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     return (
-        <main className="font-work-sans">
+        <main className="font-work-sans bg-gray-200">
             <Navbar />
             {children}
             <Footer />
+            <Toaster />
         </main>
     )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import { getProfessors } from "@/lib/actions"
 
 export default function Home() {
 
   return (
     <>
-      <h1 className="heading">Home</h1>
+      <h1 className="heading ">Gerenciamento de Professores & Cursos</h1>
     </>
   )
 }
