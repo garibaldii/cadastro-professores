48328960391f36c556ff5d91280b758f496dea1e
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-10-02 22:07:10 -0300

    feat: enhance professor management functions with update and delete actions

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,8 +4,6 @@ import { cookies } from "next/headers";
 import { apiFetch } from "./api";
 import { jwtDecode } from "jwt-decode";
 
-
-
 //professor
 export async function saveProfessor(form: FormData) {
   try {
@@ -13,8 +11,8 @@ export async function saveProfessor(form: FormData) {
 
     const res = await apiFetch("/professores", {
       method: "POST",
-      body: JSON.stringify(body)
-    })
+      body: JSON.stringify(body),
+    });
 
     if (!res.ok) {
       const errorData = await res.json();
@@ -28,45 +26,80 @@ export async function saveProfessor(form: FormData) {
   }
 }
 
-export async function getProfessors() {
+export async function updateProfessor(id: number, form: FormData) {
+  try {
+    const body = Object.fromEntries(form.entries());
 
+    const res = await apiFetch(`/professores/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(body),
+    });
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(errorData?.erro || "Erro ao atualizar professor");
+    }
+
+    return await res.json();
+  } catch (error: any) {
+    console.error("Erro na action:", error);
+    return { error: error.message || "Erro inesperado", status: "ERROR" };
+  }
+}
+
+export async function getProfessors() {
   try {
     const res = await apiFetch("/professores", {
       method: "GET",
       headers: { "Content-Type": "application/json" },
-
-    })
+    });
 
     if (!res.ok) {
       const errorData = await res.json();
       return { error: errorData.message ?? "Erro ao salvar", status: "ERROR" };
     }
 
-    const data = await res.json()
+    const data = await res.json();
 
     return data.data;
-
   } catch (error) {
     console.error("Erro na action: ", error);
     return { error: "erro inesperado", status: "ERROR" };
   }
-
-
-
 }
 
 export async function getProfessorById(id: number) {
   try {
     const res = await apiFetch(`/professores/${id}`, {
       method: "GET",
-    })
+    });
 
-    const data = await res.json()
+    const data = await res.json();
 
-    return data.data
+    return data.data;
   } catch (error) {
     console.error("Erro na action: ", error);
     return { error: "erro inesperado", status: "ERROR" };
+  }
+}
+
+export async function deleteProfessor(id: number) {
+  try {
+    const res = await apiFetch(`/professores/${id}`, {
+      method: "DELETE",
+    });
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(errorData.mensagem);
+    }
+
+    // Se não houver corpo, retorna null
+    const text = await res.text();
+    return text ? JSON.parse(text) : null;
+  } catch (error) {
+    console.error("Erro na action: ", error);
+    throw error;
   }
 }
 
@@ -93,31 +126,33 @@ export async function saveUser(form: FormData) {
 }
 
 export async function login(form: FormData) {
-
   async function setAuthToken(token: string) {
     (await cookies()).set("token", token, {
       httpOnly: true,
-      sameSite: 'lax',
-      path: '/'
-    })
+      sameSite: "lax",
+      path: "/",
+    });
   }
 
   try {
     const body = Object.fromEntries(form.entries());
-    const res = await apiFetch('/auth/login', {
+    const res = await apiFetch("/auth/login", {
       method: "POST",
       body: JSON.stringify(body),
     });
 
     if (!res.ok) {
       const errorData = await res.json();
-      console.log(JSON.stringify(errorData))
-      return { error: errorData.message ?? "Erro ao salvar " + errorData.message, status: "ERROR" };
+      console.log(JSON.stringify(errorData));
+      return {
+        error: errorData.message ?? "Erro ao salvar " + errorData.message,
+        status: "ERROR",
+      };
     }
 
-    const data = await res.json()
+    const data = await res.json();
 
-    await setAuthToken(data.token)
+    await setAuthToken(data.token);
 
     return { error: "", status: "SUCCESS", data: data };
   } catch (error) {
@@ -129,10 +164,10 @@ export async function login(form: FormData) {
 async function getUserById(id: string) {
   try {
     const res = await apiFetch(`/usuarios/${id}`, {
-      method: "GET"
-    })
+      method: "GET",
+    });
 
-    return res.json()
+    return res.json();
   } catch (error) {
     console.error("Erro na action: ", error);
     return { error: "erro inesperado", status: "ERROR" };
@@ -162,31 +197,29 @@ export async function saveCourse(body: Record<string, any>) {
 export async function deleteCourse(id: number) {
   try {
     const res = await apiFetch(`/cursos/${id}`, {
-      method: "DELETE"
-    })
+      method: "DELETE",
+    });
 
     if (!res.ok) {
-      const errorData = await res.json()
-      throw new Error(errorData.mensagem)
+      const errorData = await res.json();
+      throw new Error(errorData.mensagem);
     }
 
     // Se não houver corpo, retorna null
-    const text = await res.text()
-    return text ? JSON.parse(text) : null
-
+    const text = await res.text();
+    return text ? JSON.parse(text) : null;
   } catch (error) {
-    console.error("Erro na action: ", error)
-    throw error
+    console.error("Erro na action: ", error);
+    throw error;
   }
 }
 
 export async function updateCourse(id: number, body: Record<string, any>) {
-
   try {
     const res = await apiFetch(`/cursos/${id}`, {
       method: "PUT",
-      body: JSON.stringify(body)
-    })
+      body: JSON.stringify(body),
+    });
 
     if (!res.ok) {
       const errorData = await res.json();
@@ -198,24 +231,22 @@ export async function updateCourse(id: number, body: Record<string, any>) {
     console.error("Erro na action: ", error);
     throw error;
   }
-
 }
 
 export async function getCourses() {
   try {
     const res = await apiFetch("/cursos", {
       method: "GET",
-    })
+    });
 
     if (!res.ok) {
       const errorData = await res.json();
       return { error: errorData.message ?? "Erro ao salvar", status: "ERROR" };
     }
 
-    const data = await res.json()
+    const data = await res.json();
 
     return data.data;
-
   } catch (error) {
     console.error("Erro na action: ", error);
     return { error: "erro inesperado", status: "ERROR" };
@@ -226,17 +257,16 @@ export async function getCourseById(id: number) {
   try {
     const res = await apiFetch(`/cursos/${id}`, {
       method: "GET",
-    })
+    });
 
-    const data = await res.json()
+    const data = await res.json();
 
-    return data.data
+    return data.data;
   } catch (error) {
     console.error("Erro na action: ", error);
     return { error: "erro inesperado", status: "ERROR" };
   }
 }
-
 
 //utilitarios professor
 export async function getTitulacoes() {
@@ -279,46 +309,39 @@ export async function getStatusAtividade() {
   }
 }
 
-
 //utilitarios usuario
 
 export async function getSession() {
-
   interface DecodedToken {
-    userId: string,
-    userName: string,
-    email: string
+    userId: string;
+    userName: string;
+    email: string;
   }
 
-  const cookieStore = cookies()
-  const token = (await cookieStore).get("token")?.value
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("token")?.value;
 
   if (token) {
     const payload = jwtDecode<DecodedToken>(token);
 
-    const user = await getUserById(payload.userId)
+    const user = await getUserById(payload.userId);
 
-    return user.data
+    return user.data;
   } else {
-    throw new Error("Token inválido")
-
+    throw new Error("Token inválido");
   }
-
 }
-
 
 //utilitarios curso
 
 export async function getModeloCurso() {
   try {
     const res = await apiFetch("/enums/modeloCurso", {
-      method: "GET"
-    })
-    return res.json()
+      method: "GET",
+    });
+    return res.json();
   } catch (error) {
     console.error("Erro na action: ", error);
     return { error: "erro inesperado", status: "ERROR" };
   }
 }
-
-
