67dde1ef64aa1765a98ad47537ccfd0d11d63afa
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-10-03 17:04:53 -0300

    feat: add ExcelProfessorRegister component for importing data via spreadsheet

--- src/app/(root)/professor/cadastro/page.tsx
+++ src/app/(root)/professor/cadastro/page.tsx
@@ -1,17 +1,42 @@
+'use client'
+
+import ExcelProfessorRegister from "@/app/components/ExcelProfessorRegister";
 import ProfessorForm from "@/app/components/ProfessorForm";
-import React from "react";
+import { Switch } from "@/components/ui/switch";
+import React, { useState } from "react";
 
 // Página dinâmica devido ao middleware de autenticação
 export const dynamic = "force-dynamic";
 
 const CadastroProfessor = () => {
+
+  const [manualRegister, setManualRegister] = useState(true)
+
   return (
     <div>
       <section className="pink_container !min-h-[50px]">
         <h1 className="heading">Cadastre seu Professor</h1>
+        <p className="text-white">Preencha os dados corretamente.</p>
       </section>
-      <ProfessorForm />
-    </div>
+
+      <div className="flex justify-center text-center space-x-2 w-full">
+        <div>
+          <h1 className="text-2xl">Clique aqui para alterar o tipo de cadastro</h1>
+          <div className="flex-between">
+            <label htmlFor="tipoForm">
+              Cadastro Selecionado:{' '}
+              <span className="font-bold">
+                {manualRegister ? 'Manual' : 'Planilha Excel'}
+              </span>
+            </label>
+            <Switch id="tipoForm" checked={manualRegister} onCheckedChange={setManualRegister} />
+          </div>
+        </div>
+      </div>
+
+
+      {manualRegister ? (<ProfessorForm />) : (<ExcelProfessorRegister />)}
+    </div >
   );
 };
 
